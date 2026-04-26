cac1bfcb4a3e39eb0179978d24103d28ba1f7e08
ElyLuisMiguel <web.weavers.ea@gmail.com>
2026-04-26 16:37:16 -0400

    cambios seo y mejoras en la visualizacion de imagenes

--- src/features/landing/concurso/config/concurso.content.config.js
+++ src/features/landing/concurso/config/concurso.content.config.js
@@ -1,3 +1,8 @@
+import imgCreatives from "@/assets/images/concurso/creatives.png";
+import imgSaltoAngel from "@/assets/images/concurso/salto_angel.png";
+import imgCulture from "@/assets/images/concurso/culture.png";
+import imgArchitecture from "@/assets/images/concurso/architecture.png";
+
 /**
  * Concurso section text content configuration.
  */
@@ -11,26 +16,25 @@ export const CONCURSO_CONTENT = {
   cta: "Conoce más del proceso",
   images: [
     {
-      src: "/images/concurso/creatives.png",
+      src: imgCreatives,
       alt: "Talento venezolano trabajando",
       className: "col-span-12 md:col-span-6 lg:col-span-7",
     },
     {
-      src: "/images/concurso/salto_angel.png",
+      src: imgSaltoAngel,
       alt: "Salto Ángel - Paisaje emblemático",
       className: "col-span-12 md:col-span-4 lg:col-span-3",
     },
     {
-      src: "/images/concurso/culture.png",
+      src: imgCulture,
       alt: "Detalle cultural - Cacao",
       className: "col-span-12 md:col-span-8 lg:col-span-6",
     },
     {
-      src: "/images/concurso/architecture.png",
+      src: imgArchitecture,
       alt: "Arquitectura moderna - Caracas",
       className: "col-span-12 md:col-span-12 lg:col-span-3",
     },
   ]
-
 };
 
